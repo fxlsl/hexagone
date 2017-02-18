0e0c82f0fd89718ef037382fb030101221cfa7e7
fxlsl <17090073886@sina.cn>
2017-02-18 19:20:46 +0800

    add banner

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 $(function(){
-
+	var $ban = $('#ban');
+	var len = $ban.children().length;
+	var w = $ban.children().width();
+	$ban.width(len*w);
+	var n = 0;
+	var dis =0;
+	function getDis(n){
+		if(n<0){
+			n=len-1;
+			dis=(n-1)*w;
+		}else if(n>=len){
+			n=0;
+			dis=0;
+		}else{
+			dis=(n)*w;
+		}
+		return dis;
+	}
+	$('#ban').swipeLeft(function(){
+		n++;
+		$ban.css({
+			transform: 'translate('+-getDis(n)+'px)'
+		});
+	});
+	$('#ban').swipeRight(function(){
+		n--;
+		$ban.css({
+			transform: 'translate('+-getDis(n)+'px)'
+		});
+	});
 });
